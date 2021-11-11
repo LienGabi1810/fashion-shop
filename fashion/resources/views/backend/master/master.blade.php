29d08850d3fa3baa43a36f5d5a5deca7882c1a3f
honglien <nguyenthihonglien1098@gmail.com>
2021-11-12 00:10:46 +0700

    update 11/12/2021

--- fashion/resources/views/backend/master/master.blade.php
+++ fashion/resources/views/backend/master/master.blade.php
@@ -29,6 +29,7 @@
 
             <!-- javascript -->
     @section('script')
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
         <script src="js/scripts.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
